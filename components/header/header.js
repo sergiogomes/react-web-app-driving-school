95f3a12e6ff9e77ad130b956d162c02c66347ee4
sergiogomes <sergiopgomes92@gmail.com>
2020-08-08 19:07:45 -0300

    Creating header component

---
+++ components/header/header.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import utilStyles from "../../styles/utils.module.css";
+import styles from "./header.module.css";
+
+const name = "Driver ID on Purse";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <button
+          className={`${utilStyles.buttonIcon} ${utilStyles.colorPink}`}
+          onClick={onOpenMenu}
+        >
+          <FontAwesomeIcon icon="bars" />
+        </button>
+        <div className={styles.space}></div>
+        <Link href="/">
+          <a>
+            <img
+              src="/images/logo.png"
+              className={styles.headerImage}
+              alt={name}
+            />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export function onOpenMenu() {
+  console.log("Open menu!");
+}
--- components/meta/meta.js
+++ components/meta/meta.js
@@ -1,7 +1,66 @@
 export default function Meta() {
   return (
     <>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta http-equiv="Content-Language" content="en" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+      />
+
+      <meta name="application-name" content="React Web App - Driving School" />
+      <meta name="description" content="React Web App - Driving School." />
+      <meta
+        name="keywords"
+        content="next, web, mobile, responsive, design, website, work, react"
+      />
+      <meta name="reply-to" content="sergiopgomes92@gmail.com" />
+      <meta name="abstract" content="web, site, website" />
+      <meta name="author" content="SergioPow" />
+
+      <meta name="copyright" content="https://driveridpurse.vercel.app/" />
+      <meta name="Works" content="WebSite" />
+      <meta name="distribution" content="Global" />
+      <meta name="rating" content="General" />
+      <meta name="Googlebot" content="index, follow" />
+      <meta name="QQbot" content="index, follow" />
+      <meta name="Baidubot" content="index, follow" />
+      <meta name="Yahoo Slurp" content="index, follow" />
+      <meta name="Yahoobot" content="index, follow" />
+      <meta name="Amazonbot" content="index, follow" />
+      <meta name="Yahoobot" content="index, follow" />
+      <meta name="Unknownbot" content="index, follow" />
+      <meta name="MSN-media-crawl" content="index, follow" />
+      <meta name="Msnbot" content="index, follow" />
+      <meta name="robots" content="all" />
+      <meta name="SEO" content="https://driveridpurse.vercel.app/" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content="https://driveridpurse.vercel.app/" />
+      <meta name="twitter:title" content="React Web App - Driving School" />
+      <meta
+        name="twitter:description"
+        content="React Web App - Driving School"
+      />
+      <meta
+        name="twitter:image"
+        content="https://raw.githubusercontent.com/sergiogomes/react-notes-list/master/src/assets/images/notes.png"
+      />
+
+      <meta property="og:type" content="notes" />
+      <meta property="og:title" content="React Web App - Driving School" />
+      <meta
+        property="og:description"
+        content="React Web App - Driving School"
+      />
+      <meta property="og:url" content="https://driveridpurse.vercel.app/" />
+      <meta property="og:site_name" content="React Web App - Driving School" />
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/sergiogomes/react-notes-list/master/src/assets/images/notes.png"
+      />
+
       <link rel="icon" href="/favicons/favicon.ico" />
       <link
         rel="apple-touch-icon"
@@ -21,10 +80,6 @@ export default function Meta() {
         href="/favicons/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicons/site.webmanifest" />
-      <meta
-        name="description"
-        content="Learn how to build a personal website using Next.js"
-      />
     </>
   );
 }
