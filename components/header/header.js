9ea0e9446962c89f6ac0f8369c726aafd8b167c3
sergiogomes <sergiopgomes92@gmail.com>
2020-08-10 08:38:30 -0300

    Configuring Jest for testing.

--- components/header/header.js
+++ components/header/header.js
@@ -24,7 +24,7 @@ export default function Header() {
         >
           <FontAwesomeIcon icon="bars" />
         </button>
-        <div className={utilStyles.space}></div>
+        <div className={utilStyles.space2}></div>
         <Link href="/">
           <a>
             <img
@@ -34,6 +34,8 @@ export default function Header() {
             />
           </a>
         </Link>
+        <div className={utilStyles.space2}></div>
+        <p>Header</p>
       </div>
     </header>
   );
