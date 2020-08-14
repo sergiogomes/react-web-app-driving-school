2b5e2f575e60517778a800dee694b3abb4c2c26a
sergiogomes <sergiopgomes92@gmail.com>
2020-08-14 19:57:09 -0300

    Making more tests for alert component.

--- components/alert/alert.test.js
+++ components/alert/alert.test.js
@@ -1,9 +1,7 @@
 "use strict";
 
-import { mount, configure, render } from "enzyme";
+import { mount, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-// import renderer from "react-test-renderer";
-// import { shallow, render } from "enzyme";
 
 import Alert from "./alert";
 
@@ -16,5 +14,25 @@ describe("Alert Component", () => {
   it("should render without crashing", () => {
     const wrapper = mount(<Alert type="success" />);
     expect(wrapper.find("div")).toHaveLength(1);
+  });
+
+  it("should render a div with a success class", () => {
+    const wrapper = mount(<Alert type="success" />);
+    expect(wrapper.find("div.success")).toHaveLength(1);
+  });
+
+  it("should render a div with an error class", () => {
+    const wrapper = mount(<Alert type="error" />);
+    expect(wrapper.find("div.error")).toHaveLength(1);
+  });
+
+  it("should not render a div with an error class", () => {
+    const wrapper = mount(<Alert type="success" />);
+    expect(wrapper.find("div.error")).toHaveLength(0);
+  });
+
+  it("should not render a div with a success class", () => {
+    const wrapper = mount(<Alert type="error" />);
+    expect(wrapper.find("div.success")).toHaveLength(0);
   });
 });
