ce109d036b48541549b5255400164b73d66fd8db
sergiogomes <sergiopgomes92@gmail.com>
2020-08-16 23:55:12 -0300

    Creating toast component.

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -1,15 +1,21 @@
 import styles from "./alert.module.css";
-import cn from "classnames";
 
-export default function Alert({ children, type }) {
-  return (
-    <div
-      className={cn({
-        [styles.success]: type === "success",
-        [styles.error]: type === "error",
-      })}
-    >
-      {children}
-    </div>
-  );
+export default function Alert({ children, type, title, message }) {
+  if (type === "error") {
+  } else if (type === "warning") {
+    return (
+      <div
+        className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+        role="alert"
+      >
+        <p className="font-bold">{title}</p>
+        <p>{message}</p>
+        <div>{children}</div>
+      </div>
+    );
+  } else if (type === "success") {
+  } else if (type === "info") {
+  } else {
+    return <></>;
+  }
 }
